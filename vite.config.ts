b30906cae2b17da3196a08f50a115d92221f3a52
RZCoding07 <muhammadrizkylubis@students.polmed.ac.id>
2024-11-12 12:38:02 +0700

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,12 @@ import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-      '@public': path.resolve(__dirname, 'public')
     },
   },
 })
